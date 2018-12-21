1259f5c72921a015a5d874573208ad961b72a847
LilGast8 <contact@gastonbouchayer.com>
2018-12-21 20:24:03 +0100

    📊 Loading data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './image.jpg';
+import Data from './data.xml';
 
 
 
@@ -15,6 +16,8 @@ function component() {
 	myIcon.src = Icon;
 	
 	element.appendChild(myIcon);
+	
+	console.log( Data );
 		
 	return element;
 }
