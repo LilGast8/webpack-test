92800a6b5c23a0672c2d8c2c41188a3c003e6edf
LilGast8 <contact@gastonbouchayer.com>
2018-12-23 18:52:36 +0100

    🔥 Enable hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 
 module.exports = {
 	mode: 'development',
 	entry: {
-		app: './src/index.js',
-		print: './src/print.js'
+		app: './src/index.js'
 	},
 	devtool: 'inline-source-map',
 	devServer: {
-		contentBase: './dist'
+		contentBase: './dist',
+		disableHostCheck: true,
+		hot: true
 	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
 			title: 'Webpack test'
-		})
+		}),
+		new webpack.HotModuleReplacementPlugin()
 	],
 	output: {
 		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/'
+		path: path.resolve(__dirname, 'dist')
 	}
 };
